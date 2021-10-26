c43052af3bf1fe6fb3f2731a560a921183707bd6
ganeshgore <ganeshgore@users.noreply.github.com>
2021-10-26 19:31:40 +0000

    deploy: 1c7a406a77a3dad1dc86f7ea4c2b6f19fd2abf25

--- docs/_downloads/3f96ffecead4a422308d92bf50862bd1/wire_feedthrough.py
+++ docs/_downloads/3f96ffecead4a422308d92bf50862bd1/wire_feedthrough.py
@@ -6,11 +6,27 @@ Genrating feedthrough from single instance
 This example demostrates how to generate a feedthrough wire connection for
 a given scalar or vector wires.
 
+**Initial Design**
+
+.. hdl-diagram:: ../../../../examples/basic/_initial_design.v
+   :type: netlistsvg
+   :align: center
+   :module: top
+
+
+**Output1** ``wire0`` feedthough from ``inst_2_1``
 
 .. hdl-diagram:: ../../../../examples/basic/_output_wire.v
    :type: netlistsvg
    :align: center
+   :module: top
 
+**Output2** ``bus_in`` feedthrough from ``inst_1_0``
+
+.. hdl-diagram:: ../../../../examples/basic/_output_bus.v
+   :type: netlistsvg
+   :align: center
+   :module: top
 
 """
 
@@ -22,14 +38,20 @@ netlist = sdnphy.load_netlist_by_name('basic_hierarchy')
 
 top = netlist.top_instance.reference
 cable0 = next(top.get_cables("wire0"))
-inst2 = next(top.get_instances("inst_2_1"))
+inst2 = next(top.get_instances("inst_2_0"))
+
+sdn.compose(netlist, '_initial_design.v')
+
 
 top.create_feedthrough(inst2, cable0)
 sdn.compose(netlist, '_output_wire.v')
 
 
-# bus_in = next(top.get_cables("bus_in"))
-# inst1 = next(top.get_instances("inst_1_0"))
+netlist = sdnphy.load_netlist_by_name('basic_hierarchy')
 
-# top.create_feedthrough(inst1, bus_in)
-# sdn.compose(netlist, '_output_bus.v')
+top = netlist.top_instance.reference
+bus_in = next(top.get_cables("bus_in"))
+inst1 = next(top.get_instances("inst_1_0"))
+
+cables = top.create_feedthrough(inst1, bus_in)
+sdn.compose(netlist, '_output_bus.v')
