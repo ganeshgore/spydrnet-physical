1d2f464812658f64e9c29b3c1b7ed82b939f240d
Ganesh Gore <goreganesh007@gmail.com>
2021-10-28 23:33:54 -0600

    Added is_connected property

--- spydrnet_physical/ir/pin.py
+++ spydrnet_physical/ir/pin.py
@@ -10,3 +10,11 @@ class Pin(PinBase):
         Overrides the _bundle method from element class (returns port)
         '''
         return self.port
+
+    @property
+    def is_connected(self):
+        ''' Checks if this pin is connected to any wire
+
+        Checks for the connected wire if not found false
+        '''
+        return bool(self.wire)
