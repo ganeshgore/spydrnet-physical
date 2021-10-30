714f6b279036b42fb884c8fa546a8665be1d2caa
ganeshgore <ganeshgore@users.noreply.github.com>
2021-10-30 07:15:31 +0000

    deploy: 2cde2fee574db814d373a0d112f4b644ecc17e69

---
+++ docs/${GITHUB_REF}/_downloads/9ff84a1bc6dbda20d24153f94bdcfd55/extract_configuration_order.py
@@ -0,0 +1,16 @@
+"""
+====================================================
+Extracing configuration chain order from each module
+====================================================
+
+This example demonstrate how to extract configuration chain order from
+the restructured netlist
+
+"""
+
+from os import path
+import spydrnet as sdn
+import spydrnet_physical as sdnphy
+
+# TODO
+print("NotImplemented")
---
+++ docs/${GITHUB_REF}/_sources/introduction.rst.txt
@@ -0,0 +1,16 @@
+
+Introduction
+------------
+
+*SpyDrNet-Physical* is a plugin for
+`SpyDrNet <https://github.com/byuccl/spydrnet>`_ project,
+which adds functionality to perform physical design-related
+netlist transformations.
+
+.. toctree::
+   :maxdepth: 1
+
+   introduction/install
+   introduction/getting_started
+   introduction/shell_interface
+   introduction/visualisation
