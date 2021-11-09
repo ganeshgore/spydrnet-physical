45600d9c84ce7c0d08ab822eed10fe4c7fa5e65b
Ganesh Gore <goreganesh007@gmail.com>
2021-11-09 13:21:36 -0700

    Separateed base class

--- spydrnet_physical/util/__init__.py
+++ spydrnet_physical/util/__init__.py
@@ -1,6 +1,8 @@
+from spydrnet_physical.util.base_class import (OpenFPGA_Config_Generator,
+                                               OpenFPGA_Placement_Generator,
+                                               OpenFPGA_Tile_Generator)
+from spydrnet_physical.util.create_placement import initial_placement
 from spydrnet_physical.util.get_names import get_attr, get_names
-from spydrnet_physical.util.openfpga import (OpenFPGA,
-                                             OpenFPGA_Config_Generator,
-                                             OpenFPGA_Tile_Generator)
+from spydrnet_physical.util.openfpga import OpenFPGA
 from spydrnet_physical.util.tile01 import (Tile01, config_chain_01,
                                            config_chain_simple)
