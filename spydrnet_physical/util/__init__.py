5c257765e5c9f7aa15f91eb22d424b89199b72e0
Ganesh Gore <goreganesh007@gmail.com>
2021-11-11 08:23:42 -0700

    Added architecture parser

--- spydrnet_physical/util/__init__.py
+++ spydrnet_physical/util/__init__.py
@@ -4,5 +4,6 @@ from spydrnet_physical.util.base_class import (OpenFPGA_Config_Generator,
 from spydrnet_physical.util.create_placement import initial_placement
 from spydrnet_physical.util.get_names import get_attr, get_names
 from spydrnet_physical.util.openfpga import OpenFPGA
+from spydrnet_physical.util.openfpga_arch import OpenFPGA_Arch
 from spydrnet_physical.util.tile01 import (Tile01, config_chain_01,
                                            config_chain_simple)
---
+++ spydrnet_physical/util/openfpga_arch.py
@@ -0,0 +1,40 @@
+"""
+This file contains OpenFPGA Architecture parser class
+It is desigend to provide a API interface to probe arctecture related
+data
+"""
+
+import xml.etree.ElementTree as ET
+from spydrnet_physical.util.shell import launch_shell
+
+
+class OpenFPGA_Arch:
+
+    def __init__(self, vpr_arch, openfpga_arch) -> None:
+        self.vpr_arch = ET.parse(vpr_arch).getroot()
+        self.openfpga_arch = ET.parse(openfpga_arch).getroot()
+        self._pb_types = self._get_pb_types()
+
+    @property
+    def pb_types(self):
+        return self._pb_types
+
+    def _get_pb_types(self):
+        return [pb.get('name') for pb in self.vpr_arch.findall("./complexblocklist/pb_type")]
+
+    def get_layout(self):
+        '''
+        Returns available layouts in the architecture
+        '''
+        layout = {}
+        for each in self.vpr_arch.find("layout").findall("fixed_layout"):
+            layout[each.get("name")] = (int(each.get("width")),
+                                        int(each.get("height")))
+        return layout
+
+    def is_homogeneous(self):
+        """
+        Checks if
+        """
+        layout = self.vpr_arch.find("layout").findall("fixed_layout")[0]
+        print(layout.findall("clb"))
