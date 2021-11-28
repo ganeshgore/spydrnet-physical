04b686707441d5e2913f25e361cdcff05f92329b
Ganesh Gore <goreganesh007@gmail.com>
2021-11-28 12:42:42 -0700

    Added connection box partitioning

---
+++ examples/OpenFPGA/splitting_connection_box.py
@@ -0,0 +1,141 @@
+"""
+=========================
+Splitting Connection box
+=========================
+
+This example demonstrate, how connection box can be split into two parts 
+to be merged with the respective logic blocks.
+
+It first converts the pre tech-mapped connection box netlist to a 
+networkX graph. The graph is converted to a two dimentional array and 
+passed to metis library which performs the partitioning.
+The partitioned graph is rendered in the following SVG. 
+
+To simplify the partitioning, all the global signals are stripped down 
+before converting netlist to a graph (including connections beetween 
+shift registers chain).
+
+.. image:: ../../../examples/OpenFPGA/_graph.svg
+    :width: 500px
+    :align: center
+
+"""
+
+import glob
+import logging
+import os
+import tempfile
+from itertools import chain
+from pprint import pprint
+import networkx as nx
+
+import pymetis
+import pydot
+import spydrnet as sdn
+from networkx.drawing.nx_agraph import write_dot
+from networkx.drawing.nx_pydot import to_pydot
+from spydrnet_physical.util import OpenFPGA, config_chain_simple, get_names
+from spydrnet_physical.util.get_floorplan import FloorPlanViz
+from spydrnet_physical.util.shell import launch_shell
+
+logger = logging.getLogger('spydrnet_logs')
+sdn.enable_file_logging(LOG_LEVEL='DEBUG')
+
+
+def main():
+    # Read verilog sources
+    source_files = []
+    source_files += glob.glob('homogeneous_fabric/*_Verilog/routing/*.v')
+    source_files += glob.glob('homogeneous_fabric/*_Verilog/sub_module/*.v')
+    source_files += glob.glob(
+        'homogeneous_fabric/FPGA44_Task/CustomModules/standard_cell_p*.v')
+    source_files += glob.glob('homogeneous_fabric/FPGA44_Task/CustomModules/ccff.v')
+
+    # Temporary fix to read multiple verilog files
+    with tempfile.NamedTemporaryFile(suffix=".v") as fp:
+        for eachV in source_files:
+            with open(eachV, "r") as fpv:
+                fp.write(str.encode(" ".join(fpv.readlines())))
+        fp.seek(0)
+        netlist = sdn.parse(fp.name)
+
+    # Get cbx_1__1_ module
+    cb_module = next(netlist.get_definitions("cbx_1__1_"))
+    netlist.top_instance = cb_module
+
+    # Flattern all the mux instances
+    for instance in list(cb_module.get_instances('*ipin*')):
+        logger.debug(f"Flattening {instance.name}")
+        cb_module.flatten_instance(instance)
+
+    # Remove global port and signals
+    for signal in ["ccff_tail", "ccff_head", "prog_reset", "cfg_done",
+                   "prog_clk", "chanx_left_out", "chanx_right_out"]:
+        cb_module.remove_port(next(cb_module.get_ports(signal)))
+        cb_module.remove_cable(next(cb_module.get_cables(signal)))
+
+    # Get unwanted instances
+    for instance in ["*ASSIG*", ]:
+        cb_module.remove_children_from(cb_module.get_instances(instance))
+
+    # Disconnect scan_chain
+    for dff in cb_module.get_instances("*_CCDFF_*"):
+        port = next(dff.get_ports("*Q*"))
+        if port.pins[0].wire:
+            port.pins[0].wire.disconnect_pin(port.pins[0])
+
+    # Get connectivity graph
+    G = cb_module.get_connectivity_network()
+    graph = to_pydot(G)
+
+    n_cuts, membership = pymetis.part_graph(2, adjacency=nx.to_numpy_array(G))
+    print(f"n_cuts {n_cuts}")
+    subgraph = pydot.Cluster('part1', label='', bgcolor="#c6ecba"), \
+        pydot.Cluster('part2', label='', bgcolor="#f3cfcf")
+    partitions = [[], []]
+
+    for each in subgraph:
+        graph.add_subgraph(each)
+
+    for index, color in enumerate(membership):
+        node = graph.get_node(str(index))[0]
+        node.set_color("red" if color else "green")
+        subgraph[color].add_node(node)
+        partitions[color].append(node.get_label())
+
+    for index, color in enumerate(membership):
+        if color:
+            logger.debug(graph.get_node(str(index))[0])
+    logger.debug("")
+    for index, color in enumerate(membership):
+        if not color:
+            logger.debug(graph.get_node(str(index))[0])
+
+    print("============= Parition stats =============")
+    f_str = '{:>15s} {:^4} {:^4}'
+    print(f_str.format('', 'P1', 'P2'))
+    print("==========================================")
+    print(f_str.format('pin_I', len([p for p in partitions[0] if "pin_I" in p]),
+                       len([p for p in partitions[1] if "pin_I" in p])))
+    print(f_str.format('CCDFF', len([p for p in partitions[0] if "CCDFF" in p]),
+                       len([p for p in partitions[1] if "CCDFF" in p])))
+    print(f_str.format('top_grid', len([p for p in partitions[0] if "top_grid" in p]),
+                       len([p for p in partitions[1] if "top_grid" in p])))
+    print(f_str.format('bottom_grid', len([p for p in partitions[0] if "bottom_grid" in p]),
+                       len([p for p in partitions[1] if "bottom_grid" in p])))
+
+    for side in ["top", "bottom"]:
+        for pin in range(10):
+            tag = f"{side}_ipin_{pin}"
+            print(f_str.format(tag, len([p for p in partitions[0] if tag in p]),
+                               len([p for p in partitions[1] if tag in p])))
+
+    graph.set_rankdir("LR")
+    graph.write_png('_graph.png')
+    graph.write_svg('_graph.svg')
+    sdn.compose(netlist, '_cbx_1__1_extended.v',
+                definition_list=["cbx_1__1_"], skip_constraints=True)
+
+
+if __name__ == "__main__":
+    main()
