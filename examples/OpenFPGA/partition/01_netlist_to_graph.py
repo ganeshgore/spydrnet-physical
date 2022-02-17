d110779a7525626d5f1bacadf91a6262a42e7a1b
Ganesh Gore <goreganesh007@gmail.com>
2022-02-17 13:58:09 -0700

    Better explained graph conversion to netlist

--- examples/OpenFPGA/partition/01_netlist_to_graph.py
+++ examples/OpenFPGA/partition/01_netlist_to_graph.py
@@ -5,18 +5,15 @@ Netlist to graph (networkx)
 
 This example demonstrate how to convert a netlist to a netowrkx graph.
 
+By default each node is assigned weight 1 and each edge is assigned weight 
+based on how many wires it represents. In case of bus connected single edge
+with weight equal to length of bus is assigned.
+
 **Graph representation**
 
 .. image:: ../../../../examples/OpenFPGA/partition/_nx_graph.svg
     :align: center
     :width: 40% 
-
-**Graph representation with port split**
-
-.. image:: ../../../../examples/OpenFPGA/partition/_nx_graph_split.svg
-    :align: center
-    :width: 50% 
-
 """
 
 import matplotlib.pyplot as plt
@@ -39,8 +36,13 @@ print("\n".join([f"{node:2} {nodes[node]['label']:15} {nodes[node]}"
 print(" ========== Edges ==========")
 print("\n".join(nx.generate_edgelist(graph, data=True)))
 
+# %%
+# **Regenerated graph with ports splits into individual nodes**
+#
+# .. image:: ../../../../examples/OpenFPGA/partition/_nx_graph_split.svg
+#     :align: center
+#     :width: 50%
 
-# Regenerate graph with ports splits into individual nodes
 graph = top.get_connectivity_network(split_ports=True)
 nodes = graph.nodes
 graph_dot = to_pydot(graph)
