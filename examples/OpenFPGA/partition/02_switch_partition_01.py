ca81b38d05f429efcf90e922cbed639529468436
Ganesh Gore <goreganesh007@gmail.com>
2022-02-19 15:30:19 -0700

    BugFix - Updated example

--- examples/OpenFPGA/partition/02_switch_partition_01.py
+++ examples/OpenFPGA/partition/02_switch_partition_01.py
@@ -63,32 +63,32 @@ def sort_input_channels(switch_mat, get_bias=False):
 
 def main():
     proj = '../homogeneous_fabric'
-    for indx, sb in enumerate(glob.glob(f'{proj}/*_Verilog/routing/sb_1__1_.v')):
-        module = path.splitext(path.basename(sb))[0]
+    sb = glob.glob(f'{proj}/*_Verilog/routing/sb_1__1_.v')[0]
+    module = path.splitext(path.basename(sb))[0]
 
-        # This creates switch-box rendering class
-        sb_render = RoutingRender(module, f"{proj}/FPGA44_gsb/{module}.xml")
+    # This creates switch-box rendering class
+    sb_render = RoutingRender(module, f"{proj}/FPGA44_gsb/{module}.xml")
 
-        # Print vertical connection box (CBY) information and split
-        print(" =========== CBY =========== ")
-        sw_left = sb_render.report_ipins("left", show=False)
-        sw_left[sw_left == 'x'] = "l"
-        sw_right = sb_render.report_ipins("right", show=False)
-        sw_right[sw_right == 'x'] = "r"
-        sw = np.vstack([sw_left, sw_right])
-        left_chan, right_chan = sort_input_channels(sw)
-        print(f"left_chan {sorted(left_chan)}")
-        print(f"right_chan {sorted(right_chan)}")
-        sb_render.render_ipin(sw)
-        sb_render.render_ipin(sw[:, left_chan])
-        sb_render.render_ipin(sw[:, right_chan])
-        sb_render.render_switch_pattern()
-        sb_render.render_connection_box('left', filename="_cbx_1__1_0.svg")
-        sb_render.render_connection_box('top', filename="_cbx_1__2_0.svg")
+    # Print vertical connection box (CBY) information and split
+    print(" =========== CBY =========== ")
+    sw_left = sb_render.report_ipins("left", show=False)
+    sw_left[sw_left == 'x'] = "l"
+    sw_right = sb_render.report_ipins("right", show=False)
+    sw_right[sw_right == 'x'] = "r"
+    sw = np.vstack([sw_left, sw_right])
+    left_chan, right_chan = sort_input_channels(sw)
+    print(f"left_chan {sorted(left_chan)}")
+    print(f"right_chan {sorted(right_chan)}")
+    sb_render.render_ipin(sw)
+    sb_render.render_ipin(sw[:, left_chan])
+    sb_render.render_ipin(sw[:, right_chan])
+    sb_render.render_switch_pattern()
+    sb_render.render_connection_box('left', filename="_cbx_1__1_0.svg")
+    sb_render.render_connection_box('top', filename="_cbx_1__2_0.svg")
 
-        def left_pinmap(x): return (left_chan +
-                                    [None, None, None, None] +
-                                    right_chan).index(x)
+    def left_pinmap(x): return (left_chan +
+                                [None, None, None, None] +
+                                right_chan).index(x)
 
 # %%
 # **Horizontal Connection Box**
@@ -99,28 +99,28 @@ def main():
 # .. image:: ../../../../examples/OpenFPGA/partition/_cbx_1__1_1.svg
 #     :width: 150px
 
-        # Print vertical connection box (CBX) information and split
-        print(" =========== CBX =========== ")
-        sw_top = sb_render.report_ipins("top", show=False)
-        sw_top[sw_top == 'x'] = "t"
-        sw_bottom = sb_render.report_ipins("bottom", show=False)
-        sw_bottom[sw_bottom == 'x'] = "b"
-        sw = np.vstack([sw_top, sw_bottom])
-        top_chan, bottom_chan = sort_input_channels(sw)
-        print(f"top_chan {sorted(top_chan)}")
-        print(f"bottom_chan {sorted(bottom_chan)}")
-        sb_render.render_ipin(sw)
-        sb_render.render_ipin(sw[:, top_chan])
-        sb_render.render_ipin(sw[:, bottom_chan])
+    # Print vertical connection box (CBX) information and split
+    print(" =========== CBX =========== ")
+    sw_top = sb_render.report_ipins("top", show=False)
+    sw_top[sw_top == 'x'] = "t"
+    sw_bottom = sb_render.report_ipins("bottom", show=False)
+    sw_bottom[sw_bottom == 'x'] = "b"
+    sw = np.vstack([sw_top, sw_bottom])
+    top_chan, bottom_chan = sort_input_channels(sw)
+    print(f"top_chan {sorted(top_chan)}")
+    print(f"bottom_chan {sorted(bottom_chan)}")
+    sb_render.render_ipin(sw)
+    sb_render.render_ipin(sw[:, top_chan])
+    sb_render.render_ipin(sw[:, bottom_chan])
 
-        def top_pinmap(x): return (bottom_chan +
-                                   [None, None, None, None] +
-                                   top_chan).index(x)
+    def top_pinmap(x): return (bottom_chan +
+                               [None, None, None, None] +
+                               top_chan).index(x)
 
-        sb_render.render_connection_box('left', pinmap=left_pinmap,
-                                        filename="_cbx_1__1_1.svg")
-        sb_render.render_connection_box('top', pinmap=top_pinmap,
-                                        filename="_cbx_1__2_1.svg")
+    sb_render.render_connection_box('left', pinmap=left_pinmap,
+                                    filename="_cbx_1__1_1.svg")
+    sb_render.render_connection_box('top', pinmap=top_pinmap,
+                                    filename="_cbx_1__2_1.svg")
 
 
 if __name__ == "__main__":
