e0cdeb79dacd854b9b3bb9fb2d5299fd344d4583
ganeshgore <ganeshgore@users.noreply.github.com>
2022-03-13 22:15:25 +0000

    deploy: 2402ce9902fb73e24d3c9bab6a3e44291e3d1498

--- docs/_downloads/7fd72747e3bce38e621819500c8aa7a4/03_create_htree.py
+++ docs/_downloads/7fd72747e3bce38e621819500c8aa7a4/03_create_htree.py
@@ -13,8 +13,14 @@ arbitrary (non square ) tiles.
     :width: 500px
     :align: center
 
-"""
+.. image:: ../../../../examples/OpenFPGA/clock_tree/_hybrid_connectivity_pattern_graph.svg
+    :height: 500px
+    :align: center
 
+"""
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_pydot import to_pydot
 from spydrnet_physical.util import ConnectionPattern
 
 WIDTH = 19
@@ -41,6 +47,47 @@ for x in range(3, WIDTH-1):
     else:
         hyb_pat.cursor = (int(WIDTH/2)+1, int(HEIGHT/2)+2)
         hyb_pat.move_y(steps=int(HEIGHT/2)-1)
+
+
+graph = hyb_pat.create_graph()
+
+# TODO Annotate graph with wire length  from source
+
+
+def leaf_node_weight(x, y):
+    return 4
+
+
+for node_lbl in graph.nodes:
+    if graph.out_degree(node_lbl) == 0:
+        _, x, y, _ = node_lbl.split("_")
+        node = graph.nodes[node_lbl]
+        node["weight"] = leaf_node_weight(x, y)
+        node["color"] = "red"
+        node["label"] = f"{node_lbl}_{leaf_node_weight(x, y)}"
+
+
+def get_leaf_sum(graph, node):
+    total_weight = 0
+    for node_lbl in nx.dfs_preorder_nodes(graph, node):
+        if node_lbl == node:
+            continue
+        node = graph.nodes[node_lbl]
+        if "weight" in node:
+            total_weight += int(node["weight"])
+    return total_weight
+
+
+for node_lbl in nx.dfs_preorder_nodes(graph):
+    if graph.out_degree(node_lbl) > 1:
+        node = graph.nodes[node_lbl]
+        node["label"] = f"{node_lbl}_[{get_leaf_sum(graph, node_lbl)}]"
+
+
+graph = to_pydot(graph)
+graph.write_dot("_hybrid_connectivity_pattern_graph.dot")
+graph.write_svg("_hybrid_connectivity_pattern_graph.svg")
+graph.write_png("_hybrid_connectivity_pattern_graph.png")
 
 svg = cpat.render_pattern(title="Hybrid Pattern")
 svg.saveas("_hybrid_connectivity_pattern.svg", pretty=True, indent=4)
