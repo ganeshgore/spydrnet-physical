fffb31c0ceb90517548669681dd3a46b1dc7839d
Ganesh Gore <goreganesh007@gmail.com>
2022-03-20 20:01:04 -0600

    Added Cython installation in build_docs

--- .github/build_docs.sh
+++ .github/build_docs.sh
@@ -6,3 +6,4 @@ python3 -m pip install --upgrade --no-cache-dir pip
 if [ -f "docs/requirements.txt" ]; then
     python3 -m pip install --upgrade --no-cache-dir -r docs/requirements.txt
 fi
+pip install --upgrade Cython
