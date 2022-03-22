9149c8ea4745d644cce8d18625c27cc29aa42808
ganeshgore <ganeshgore@users.noreply.github.com>
2022-03-22 06:26:19 +0000

    deploy: 69268d91d04403c6728235dd9782e11b27cf512f

--- docs/pre_release/_downloads/3b15d0eef986f127b49c2badcd130fee/07_FloorplanHeterogeneousDesign01.py
+++ docs/pre_release/_downloads/3b15d0eef986f127b49c2badcd130fee/07_FloorplanHeterogeneousDesign01.py
@@ -9,7 +9,7 @@ based on ``initial_placement`` class.
 
 This script can be used for shaping and placement of the modules before place and route.
 
-.. image:: ../../../../examples/OpenFPGA/basic/_design_floorplan.svg
+.. image:: ../../../../examples/OpenFPGA/basic/_hetero_design_floorplan.svg
    :width: 70%
    :align: center
 
