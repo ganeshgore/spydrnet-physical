5bbff881087dd7416f9abf8eb363e594842f90d4
Ganesh Gore <goreganesh007@gmail.com>
2022-05-27 17:10:59 -0600

    Moved shaping related static methods to separate file

--- spydrnet_physical/ir/definition.py
+++ spydrnet_physical/ir/definition.py
@@ -2,14 +2,16 @@
 import logging
 import typing
 from itertools import combinations
-from spydrnet_physical.global_state import global_callback as sdnphy_global_callback
+
 import numpy as np
 import spydrnet as sdn
 from spydrnet.ir import Definition as DefinitionBase
 from spydrnet.ir.innerpin import InnerPin
 from spydrnet.ir.outerpin import OuterPin
 from spydrnet.ir.port import Port
-
+from spydrnet_physical.global_state import \
+    global_callback as sdnphy_global_callback
+from spydrnet_physical.ir.shaping_utils import shaping_utils
 
 logger = logging.getLogger('spydrnet_logs')
 try:
@@ -39,31 +41,6 @@ class Definition(DefinitionBase):
         self.properties["WIDTH"] = properties.get("WIDTH", 50)
         self.properties["HEIGHT"] = properties.get("WIDTH", 50)
 
-    @staticmethod
-    def get_custom_boundary(points):
-        path = points.split()
-        direction = path[0].lower()
-        origin = path[1:3]
-        boundary = [int(origin[0]), int(origin[1])]
-        for pt in map(int, map(float, path[3:])):
-            if direction == 'v':
-                # print("v")
-                boundary.extend([boundary[-2], boundary[-1]+pt])
-                direction = 'h'
-            else:
-                # print("h")
-                boundary.extend([boundary[-2]+pt, boundary[-1]])
-                direction = 'v'
-            # print(boundary)
-        return boundary
-
-    @staticmethod
-    def PolyArea2D(pts):
-        pts = list(zip(pts[::2], pts[1::2]))
-        lines = np.hstack([pts, np.roll(pts, -1, axis=0)])
-        area = 0.5*abs(sum(x1*y2-x2*y1 for x1, y1, x2, y2 in lines))
-        return area
-
     @property
     def area(self):
         shape = self.data["VERILOG.InlineConstraints"].get("SHAPE", None)
@@ -73,7 +50,7 @@ class Definition(DefinitionBase):
                     "POINTS", [0, 0, 0, 0, 0, 0])
             return ((a+f+c) * d) + ((b+d+e) * a) - (d*a)
         elif shape == "custom":
-            return self.PolyArea2D(self.get_custom_boundary(
+            return shaping_utils.PolyArea2D(shaping_utils.get_custom_boundary(
                 self.data["VERILOG.InlineConstraints"].get("POINTS",
                                                            [0, 0, 0, 0, 0, 0])
             ))
@@ -301,168 +278,6 @@ class Definition(DefinitionBase):
         return main_def, instance_list
 
     @staticmethod
-    def _convert_rect_to_pt(inst):
-        """
-        Returns the absolute points of the instance with rect shape
-        """
-        LOC_X = inst.data[PROP].get("LOC_X", 0)
-        LOC_Y = inst.data[PROP].get("LOC_Y", 0)
-        WIDTH = inst.reference.data[PROP].get("WIDTH", 0)
-        HEIGHT = inst.reference.data[PROP].get("HEIGHT", 0)
-        return [(LOC_X, LOC_Y),
-                (LOC_X, LOC_Y+HEIGHT),
-                (LOC_X+WIDTH, LOC_Y+HEIGHT),
-                (LOC_X+WIDTH, LOC_Y)]
-
-    @staticmethod
-    def _convert_cross_to_pt(inst):
-        """
-        Returns the absolute points of the instance with cross shape
-        """
-        loc_x = inst.data[PROP].get("LOC_X", 0)
-        loc_y = inst.data[PROP].get("LOC_Y", 0)
-        a, b, c, d, e, f = inst.reference.data[PROP].get("POINTS",
-                                                         [10, 10, 10, 10, 10, 10])
-        return [(loc_x+b, loc_y),
-                (loc_x+b, loc_y+f),
-                (loc_x, loc_y+f),
-                (loc_x, loc_y+f+a),
-                (loc_x+b, loc_y+f+a),
-                (loc_x+b, loc_y+a+f+c),
-                (loc_x+b+d, loc_y+a+f+c),
-                (loc_x+b+d, loc_y+a+f),
-                (loc_x+b+d+e, loc_y+a+f),
-                (loc_x+b+d+e, loc_y+f),
-                (loc_x+b+d, loc_y+f),
-                (loc_x+b+d, loc_y)]
-
-    @staticmethod
-    def _get_outline(points):
-        """
-        """
-        min_x = min([x for x, _ in points])
-        min_y = min([y for _, y in points])
-        return points
-
-    @staticmethod
-    def _get_orientation(origin, p1, p2):
-        difference = (
-            ((p2[0] - origin[0]) * (p1[1] - origin[1]))
-            - ((p1[0] - origin[0]) * (p2[1] - origin[1])))
-        return difference
-
-    @staticmethod
-    def _points_to_path(points):
-        """
-        Converts the list of outline points to strin representing custom shape
-
-        .. note:: Points should be in the correct order to obtain string
-
-        Args:
-            points list((float, float)) : list of x and y points
-
-        """
-        sequence_string = ""
-        sequence_string += "V" if points[0][0] == points[1][0] else "H"
-        sequence_string += " %d %d" % (0, 0)
-        pt1 = points[0]
-        for pt2 in points[1:]:
-            dx = pt2[0] - pt1[0]
-            dy = pt2[1] - pt1[1]
-            pt1 = pt2
-            sequence_string += " %d" % (dx+dy)
-        return sequence_string
-
-    @staticmethod
-    def _orientation(origin, p1, p2):
-        difference = (((p2[0] - origin[0]) * (p1[1] - origin[1]))
-                      - ((p1[0] - origin[0]) * (p2[1] - origin[1])))
-        return difference
-
-    @staticmethod
-    def _get_shapes_outline(array):
-        """
-        Traces the outline of the given object
-
-        While tracing the outline it enforces rectilinear conenctions
-
-        Args:
-            points list((float, float)) : list of x and y points
-
-        """
-        points = []
-        [points.append(x) for x in array if x not in points]
-        _hull_points = []
-
-        start = [pt for pt in points if pt[0]
-                 == min([x for x, _ in points])][0]
-        point = start
-        _hull_points.append(start)
-
-        far_point = None
-        while (far_point is not start):
-
-            # get the first point (initial max) to use to compare with others
-            p1 = None
-            for p in points:
-                if not p is point:
-                    p1 = p
-                    break
-
-            far_point = p1
-
-            for p2 in points:
-                # Ensure we aren't comparing to self or pivot point
-                if not (p2 is point or p2 is p1):
-                    direction = Definition._orientation(point, far_point, p2)
-                    if direction > 0:
-                        far_point = p2
-            # Get delta_x and delta_y of current point with previous
-            delta_x = far_point[0] - point[0]
-            delta_y = far_point[1] - point[1]
-            # Check if its not horizontal or vertical connection
-            # force it to be horizontal or vertical connetion
-            if (delta_x*delta_y):
-                delta_x = delta_x/abs(delta_x) if delta_x else 0
-                delta_y = delta_y/abs(delta_y) if delta_y else 0
-                # Find new intermediate point
-                new_pt = \
-                    (far_point[0], point[1]) if (delta_x, delta_y) == (-1, 1) else \
-                    (far_point[0], point[1]) if (delta_x, delta_y) == (1, -1) else \
-                    (point[0], far_point[1]) if (delta_x, delta_y) == (1, 1) else \
-                    (point[0], far_point[1]) if (delta_x, delta_y) == (-1, -1) else \
-                    (None, None)
-                # Add intermediate point and current point
-                _hull_points.append(new_pt)
-                _hull_points.append(far_point)
-            else:
-                _hull_points.append(far_point)
-            if len(_hull_points) > 2:
-                # IF three points in line remove middle point
-                pt1, pt, pt2 = _hull_points[-3:]
-                if (pt1[0] == pt[0] == pt2[0]) or (pt1[1] == pt[1] == pt2[1]):
-                    _hull_points.remove(pt)
-            point = far_point
-        # check if first point is co linear with second an last point
-        pt1, pt, pt2 = _hull_points[-2], _hull_points[0], _hull_points[1]
-        if (pt1[0] == pt[0] == pt2[0]) or (pt1[1] == pt[1] == pt2[1]):
-            _hull_points.remove(pt) 
-            _hull_points[-1] = _hull_points[0]
-        return "custom", _hull_points
-
-    @staticmethod
-    def _interpret_custom_to_shape(new_instance):
-        shape = new_instance.reference.properties.get("SHAPE", None)
-        if not shape == "custom":
-            return
-        points = new_instance.reference.properties.get("POINTS", None)
-        points = points.split()
-        if len(points) == 7:
-            new_instance.reference.properties["SHAPE"] = "rect"
-            new_instance.reference.properties["WIDTH"] = int(points[3])
-            new_instance.reference.properties["HEIGHT"] = int(points[4])
-
-    @staticmethod
     def _call_merged_instance(new_mod, new_instance, instances_list):
         # def_data = instances_list[0].data["VERILOG.InlineConstraints"]
         # if def_data:
@@ -471,9 +286,9 @@ class Definition(DefinitionBase):
         for each in instances_list:
             shape = each.reference.data[PROP].get("SHAPE", None)
             if shape == "rect":
-                outline.extend(Definition._convert_rect_to_pt(each))
+                outline.extend(shaping_utils._convert_rect_to_pt(each))
             if shape == "cross":
-                outline.extend(Definition._convert_cross_to_pt(each))
+                outline.extend(shaping_utils._convert_cross_to_pt(each))
             new_mod.data[PROP]["AREA"] = each.reference.data[PROP].get(
                 "AREA", 0)
         LOC_X = min([each.data[PROP].get("LOC_X", 0)
@@ -483,23 +298,23 @@ class Definition(DefinitionBase):
         new_instance.data[PROP]["LOC_X"] = LOC_X
         new_instance.data[PROP]["LOC_Y"] = LOC_Y
         if outline:
-            shape, points = Definition._get_shapes_outline(outline)
+            shape, points = shaping_utils._get_shapes_outline(outline)
             new_instance.reference.properties["SHAPE"] = shape
             if shape == "cross":
                 new_instance.reference.properties["POINTS"] = points
             if shape == "custom":
                 new_instance.reference.properties["POINTS"] = \
-                    Definition._points_to_path(points)
+                    shaping_utils._points_to_path(points)
             if shape == "rect":
                 new_instance.reference.properties["WIDTH"] = points[0]
                 new_instance.reference.properties["HEIGHT"] = points[1]
-            Definition._interpret_custom_to_shape(new_instance)
+            shaping_utils._interpret_custom_to_shape(new_instance)
 
             logger.debug(f"{new_instance.name} " +
-                  f"[{new_instance.reference.name:15}]" +
-                  f"[{new_instance.reference.properties['SHAPE']:15}]" +
-                  f"[{new_instance.reference.properties['WIDTH']:15}]" +
-                  f" {shape} {points}")
+                         f"[{new_instance.reference.name:15}]" +
+                         f"[{new_instance.reference.properties['SHAPE']:15}]" +
+                         f"[{new_instance.reference.properties['WIDTH']:15}]" +
+                         f" {shape} {points}")
 
     # TODO: Try to break this method
     def merge_instance(self, instances_list,
