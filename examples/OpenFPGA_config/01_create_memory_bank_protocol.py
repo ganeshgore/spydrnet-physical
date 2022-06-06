779d1c8994cb175fa220ca9ef1813d90ea2b41d7
Ganesh Gore <goreganesh007@gmail.com>
2022-06-06 12:16:29 -0600

    Updated MB example

--- examples/OpenFPGA_config/01_create_memory_bank_protocol.py
+++ examples/OpenFPGA_config/01_create_memory_bank_protocol.py
@@ -28,23 +28,13 @@ from the following tcl script.
 - Sort each cell with increasing order of ``y_loc``,
   assign wl[0] to first set of ``bl_n`` cells and continue assignement till the end
 
-**Highlight WL connectivity**:
-
-.. image:: ../../../examples/OpenFPGA_config/_memeory_bank_conn_wl_lines.svg
-    :width: 80%
-    :align: center
-
 **Highlight BL connectivity**:
 
 .. image:: ../../../examples/OpenFPGA_config/_memeory_bank_conn_bl_line.svg
     :width: 80%
     :align: center
 
-**Output tcl**:
-
-.. literalinclude:: ../../../examples/OpenFPGA_config/_eco_changes.tcl
-    :language: tcl
-    :lines: 1-10
+**Output log**:
 
 .. literalinclude:: ../../../examples/OpenFPGA_config/_memory_bank_protocol_spydrnet.log
     :language: text
@@ -63,20 +53,23 @@ import svgwrite
 
 logger = logging.getLogger('spydrnet_logs')
 sdn.enable_file_logging(LOG_LEVEL='INFO', filename="memory_bank_protocol")
+logger.handlers[1].setFormatter(logging.Formatter(
+    "%(levelname)5s %(lineno)3s - %(message)s"))
 
 
 def main():
-    latch_locs = pd.read_csv('small_latch_placement.txt', sep=" ",
+    latch_locs = pd.read_csv('latch_placement_dump.txt', sep=" ",
                              names=["cells", "x_loc", "y_loc"])
+    # latch_locs = pd.read_csv('small_latch_placement.txt', sep=" ",
+    #                          names=["cells", "x_loc", "y_loc"])
     np.random.seed(40)
-    latch_locs = latch_locs.sample(n=15)
-
+    # latch_locs = latch_locs.sample(n=750)
     total_mem_elements = latch_locs.shape[0]
     wl_n = math.ceil(math.sqrt(total_mem_elements))
     bl_n = math.ceil(total_mem_elements/wl_n)
 
-    x_min, x_max = int(min(latch_locs["x_loc"])), int(max(latch_locs["x_loc"]))
-    y_min, y_max = int(min(latch_locs["y_loc"])), int(max(latch_locs["y_loc"]))
+    _, x_max = int(min(latch_locs["x_loc"])), int(max(latch_locs["x_loc"]))
+    _, y_max = int(min(latch_locs["y_loc"])), int(max(latch_locs["y_loc"]))
 
     logger.info("Total mem elements %s", total_mem_elements)
     logger.info("word lines         %s", wl_n)
@@ -85,12 +78,10 @@ def main():
     logger.info("Extra Conns        %s", (wl_n*bl_n)-total_mem_elements)
 
     latch_locs = latch_locs.sort_values(by=['x_loc', 'y_loc'])
-    print(latch_locs.head())
     x_cuts = latch_locs[::wl_n]['x_loc'].values.tolist() + [x_max]
     bl_lines = [round(sum(pts)*0.5, 2) for pts in zip(x_cuts[:-1], x_cuts[1:])]
 
-    sequence = [j for j in range(bl_n) for i in range(bl_n)]
-    print(sequence)
+    sequence = [j for j in range(bl_n) for i in range(wl_n)]
     latch_locs["x_bins"] = sequence[:latch_locs.shape[0]]
 
     latch_locs = latch_locs.sort_values(by=['x_bins', 'y_loc'])
@@ -107,21 +98,24 @@ def main():
     y_cuts += [y_max]
     wl_lines = [round(sum(pts)*0.5, 2) for pts in zip(y_cuts[:-1], y_cuts[1:])]
 
+    # This is an alternate option to place WL pins based on the
+    # the average y location of each Wx assigned cells
+    # wl_lines = []
+    # for each in sorted(latch_locs["wl"].unique()):
+    #     rows = latch_locs[latch_locs["wl"] == each]["y_loc"].values
+    #     print(each, rows)
+    #     wl_lines.append(sum(rows)/len(rows))
+
     logger.info("x_cuts     %s", x_cuts)
-    logger.info("y_cuts     %s", y_cuts)
     logger.info("wl_lines   %s", wl_lines)
     logger.info("bl_lines   %s", bl_lines)
-    for each in latch_locs["wl"].unique():
-        logger.info("wl_line [%s]   %d", each, sum(latch_locs["wl"] == each))
-    for each in latch_locs["bl"].unique():
-        logger.info("bl_line [%s]   %d", each, sum(latch_locs["bl"] == each))
 
-    render_placement(latch_locs, wl_lines, bl_lines, None, None,
-                     filename="_memeory_bank_conn_wl_lines.svg", highlights="w")
-    render_placement(latch_locs, wl_lines, bl_lines, None, None,
+    render_placement(latch_locs, wl_lines, bl_lines, x_cuts,
                      filename="_memeory_bank_conn_bl_line.svg", highlights="b")
     write_report(latch_locs)
     validate_connection(latch_locs)
+    cost = find_connection_cost(latch_locs, wl_lines, bl_lines)
+    logger.info("V Cost %.2f, H Cost %.2f, Total Cost %.2f", *cost, sum(cost))
 
 
 def write_report(latch_locs):
@@ -175,12 +169,11 @@ def find_connection_cost(latch_locs, wl_lines, bl_lines):
         bl_indx = int(each['bl'][1:])
         total_h_wiring = abs(x_loc-wl_lines[wl_indx])
         total_v_wiring = abs(y_loc-bl_lines[bl_indx])
-    return total_v_wiring, total_h_wiring
+    return round(total_v_wiring, 2), round(total_h_wiring, 2)
 
 
 def render_placement(latch_locs, wl_lines, bl_lines,
-                     x_cuts=None, y_cuts=None,
-                     filename="_sample.svg", highlights="b"):
+                     regions=None, filename="_sample.svg", highlights="b"):
     dwg = svgwrite.Drawing()
     _, xmax = min(latch_locs["x_loc"]), max(latch_locs["x_loc"])+8
     _, ymax = min(latch_locs["y_loc"]), max(latch_locs["y_loc"])+8
@@ -214,14 +207,14 @@ def render_placement(latch_locs, wl_lines, bl_lines,
                               class_="connections"))
 
     # Add word and bit lines regions
-    if x_cuts:
-        for indx, pts in enumerate(zip(x_cuts[:-1], x_cuts[1:])):
+    if regions and (highlights == "b"):
+        for indx, pts in enumerate(zip(regions[:-1], regions[1:])):
             min_pt, max_pt = pts
             dwg_main.add(dwg.rect(size=((max_pt-min_pt), ymax), class_="bl_region",
                                   fill=palette[indx],
                                   opacity=0.1, insert=(min_pt, -4)))
-    if y_cuts:
-        for indx, pts in enumerate(zip(y_cuts[:-1], y_cuts[1:])):
+    if regions and (highlights == "w"):
+        for indx, pts in enumerate(zip(regions[:-1], regions[1:])):
             min_pt, max_pt = pts
             dwg_main.add(dwg.rect(size=(xmax, (max_pt-min_pt)), class_="wl_region",
                                   fill=palette[indx],
@@ -243,3 +236,10 @@ def render_placement(latch_locs, wl_lines, bl_lines,
 
 if __name__ == "__main__":
     main()
+
+# %%
+# **Output TCL**
+#
+# .. literalinclude:: ../../../examples/OpenFPGA_config/_eco_changes.tcl
+#     :language: tcl
+#     :lines: 1-15
