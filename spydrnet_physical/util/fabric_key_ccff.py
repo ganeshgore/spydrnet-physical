b0ae22aa54cf5f7fbc64a3ec9e52783bbd553fa6
Ganesh Gore <goreganesh007@gmail.com>
2022-06-06 21:36:17 -0600

    Added simple connection structure

--- spydrnet_physical/util/fabric_key_ccff.py
+++ spydrnet_physical/util/fabric_key_ccff.py
@@ -52,13 +52,24 @@ class FabricKeyGenCCFF:
         match = re.match(regex, instance_name)
         return match.group(1), match.group(2)
 
-    def create_vertical_fabric(self):
-        for xpt in range(self.fpga_grid.get_width()+2):
-            for ypt in range(self.fpga_grid.get_height()+2):
-                inst_name = self.fpga_grid.get_top_instance(xpt, ypt)
+    def create_serpentine_connection(self):
+        for xpt in range(0, (self.fpga_grid.get_width()*2)+3, 2):
+            for ypt in range((self.fpga_grid.get_height()*2)+3):
+                try:
+                    inst_name = self.fpga_grid.get_top_instance(xpt, ypt)
+                except IndexError:
+                    break
                 if inst_name == "EMPTY":
                     continue
-                self.fkey[0] += (xpt, ypt, inst_name)
+                self.fkey[0] += [(xpt, ypt, inst_name)]
+            for ypt in range((self.fpga_grid.get_height()*2)+3)[::-1]:
+                try:
+                    inst_name = self.fpga_grid.get_top_instance(xpt+1, ypt)
+                except IndexError:
+                    break
+                if inst_name == "EMPTY":
+                    continue
+                self.fkey[0] += [(xpt+1, ypt, inst_name)]
 
     def create_fabric_key(self, pattern=None):
         '''
@@ -67,7 +78,10 @@ class FabricKeyGenCCFF:
         Args:
             pattern (str): Type of fabric key pattern
         '''
-        self.create_vertical_fabric()
+        if pattern == "serpentine":
+            self.create_serpentine_connection()
+        else:
+            self.create_serpentine_connection()
         return self.fkey
 
     def render_svg(self, filename, skip_instance="grid_io*"):
@@ -94,10 +108,15 @@ class FabricKeyGenCCFF:
             mapping[element.text] = (float(element.attribs["x"]),
                                      -1*float(element.attribs["y"]))
 
-        self.dwg_shapes.add(dwg.line(start=mapping["clb_1__1_"],
-                                     end=mapping["cby_1__1_"],
-                                     marker_end=marker.get_funciri(),
-                                     stroke_width='0.4px', stroke="red"))
+        for each_region in self.fkey:
+            points = []
+            for each_instance in each_region:
+                points += map(str, mapping[each_instance[-1]])
+            self.dwg_shapes.add(dwg.path(d=f"M {points[0]} {points[1]} " +
+                                         " ".join(points), fill="none",
+                                         marker_mid=marker.get_funciri(),
+                                         marker_end=marker.get_funciri(),
+                                         stroke_width='0.4px', stroke="red"))
 
         if filename:
             self.dwg.saveas(filename, pretty=True, indent=4)
