e08bdd963dfedab494675b6a165f8dcfa6d7597b
Saad Khalil <saad.khalil@rslpt50.rapidsilicon.local>
2022-06-28 12:09:45 +0500

    added tests to definition class

--- spydrnet_physical/ir/tests/test_definition.py
+++ spydrnet_physical/ir/tests/test_definition.py
@@ -166,7 +166,185 @@ class TestDefinition(unittest.TestCase):
     def test_get_connectivity_network():
         assert 1 == 2
 
-    @expectedFailure
+    
     def test_merge_multiple_instance(self):
-        # TODO: Wrte test for checking merge multiple instances
-        self.definition.merge_multiple_instance()
+        def2 = self.library.create_definition("def2")
+        def3 = self.library.create_definition("def3")
+        inst2 = self.definition.create_child("inst2", reference=def2)
+        inst3 = self.definition.create_child("inst3", reference=def3)
+        inst4 = self.definition.create_child("inst4", reference=def2)
+        inst5 = self.definition.create_child("inst5", reference=def3)
+
+        instance_list_tuple = [((inst2, inst3),'merged_inst_2_3'), ((inst4, inst5), "merged_inst_4_5")]
+
+        new_def, inst_list = self.definition.merge_multiple_instance(instance_list_tuple, new_definition_name = 'new_merged_inst')
+
+        merged_inst1 = inst_list[0]
+        merged_inst2 = inst_list[1]
+
+        self.assertEqual(new_def.name, 'new_merged_inst')
+        self.assertEqual(merged_inst1.name, 'merged_inst_2_3')
+        self.assertEqual(merged_inst2.name, 'merged_inst_4_5')
+
+
+    def test_combine_cables(self):
+        ''' Creates 3 port on the given definition and combines them '''
+        port1 = self.definition.create_port(pins=1)
+        port2 = self.definition.create_port(pins=1)
+        port3 = self.definition.create_port(pins=1)
+
+        cable1 = self.definition.create_cable(wires=1)
+        cable1.connect_port(port1)
+        wire1 = cable1.wires[0]
+
+        cable2 = self.definition.create_cable(wires=1)
+        cable2.connect_port(port2)
+        wire2 = cable2.wires[0]
+
+        cable3 = self.definition.create_cable(wires=1)
+        cable3.connect_port(port3)
+        wire3 = cable3.wires[0]
+
+        new_cable = self.definition.combine_cables(
+            "merged_cables", [cable1, cable2, cable3])
+
+        self.assertIsInstance(new_cable, sdn.Cable)
+        self.assertEqual(new_cable.size, 3)
+        self.assertEqual(len(self.definition.ports), 3)
+
+    def test_make_unique_instance(self):
+
+        def1 = self.library.create_definition("def1")
+        inst = self.definition.create_child("inst", reference= def1)
+
+        new_def = self.definition.make_instance_unique(inst, "inst_new")
+        
+        self.assertEqual(new_def.name, "inst_new")
+
+
+    def test_OptPins(self):
+        top_def = self.library.create_definition("top_def")
+
+        #defing the ios and cables for the top module
+        top_in0_port = top_def.create_port(name="in0",direction=sdn.IN)
+        top_in1_port = top_def.create_port(name="in1",direction=sdn.IN)
+        top_out0_port = top_def.create_port(name="out0",direction=sdn.OUT)
+
+        top_in0_pin = top_in0_port.create_pin()
+        top_in1_pin = top_in1_port.create_pin()
+        top_out0_pin = top_out0_port.create_pin()
+
+        cable_io0 = top_def.create_cable(name="in0",is_scalar=True,wires=1)
+        cable_io1 = top_def.create_cable(name = "in1", is_scalar=True, wires = 1)
+        cable_out = top_def.create_cable(name = "out0", is_scalar=True, wires = 1)
+        cable_wire = top_def.create_cable(name = "wire0", is_scalar=True, wires = 1)
+
+        cable_io0.connect_port(top_in0_port,reverse=False)
+        cable_io1.connect_port(top_in1_port,reverse=False)
+        cable_out.connect_port(top_out0_port,reverse=False)
+
+        #defing module 1
+        def1 = self.library.create_definition("Module1")
+        #defining the ports and pins
+        def1_in0 = def1.create_port("in0", direction=sdn.IN)
+        def1_in1 = def1.create_port("in1", direction=sdn.IN)
+        def1_out = def1.create_port("out0", direction=sdn.OUT)
+
+        mod1_in0_pin = def1_in0.create_pin()
+        mod1_in1_pin = def1_in1.create_pin()
+        mod1_out0_pin = def1_out.create_pin()
+
+        cable_mod1in0 = def1.create_cable(name="in0",is_scalar=True,wires=1)
+        cable_mod1in1 = def1.create_cable(name = "in1", is_scalar=True, wires = 1)
+        cable_mod1out = def1.create_cable(name = "out", is_scalar=True, wires = 1)
+
+        #creating the child instances
+        inst1 = top_def.create_child("inst1", reference= def1)
+        for ports in inst1.reference.get_ports():
+            if ports.name == 'in0':
+                inst_pin = inst1.pins[ports.pins[0]]
+                next(top_def.get_cables("in0")).wires[0].connect_pin(inst_pin)
+            elif ports.name == 'in1': 
+                inst_pin = inst1.pins[ports.pins[0]]   
+                next(top_def.get_cables("in1")).wires[0].connect_pin(inst_pin)
+            else:
+                inst_pin = inst1.pins[ports.pins[0]]
+                next(top_def.get_cables("wire0")).wires[0].connect_pin(inst_pin)
+
+        inst2 = top_def.create_child("inst2", reference= def1)
+        for ports in inst2.reference.get_ports():
+            if ports.name == 'in0':
+                inst_pin = inst2.pins[ports.pins[0]]
+                next(top_def.get_cables("wire0")).wires[0].connect_pin(inst_pin)
+            elif ports.name == 'in1': 
+                inst_pin = inst2.pins[ports.pins[0]]   
+                next(top_def.get_cables("in0")).wires[0].connect_pin(inst_pin)
+            else:
+                inst_pin = inst2.pins[ports.pins[0]]
+                next(top_def.get_cables("out0")).wires[0].connect_pin(inst_pin)
+
+
+        new_m, inst, pin_map = top_def.merge_instance([inst1, inst2])
+        port_list = [print(i.name) for i in new_m.ports]
+        self.assertEqual(len(port_list), 6)
+        #with merge = True
+        new_m.OptPins()
+        port_list = [print(i.name) for i in new_m.ports]
+        self.assertEqual(len(port_list), 3)   
+
+
+    def test_split_port(self):
+        port = self.definition.create_port(name="in0", is_downto=True, is_scalar = False, lower_index = 0, direction=sdn.IN, pins=4)
+
+        cable = self.definition.create_cable(name="in0",is_downto=True, is_scalar = False, lower_index = 0, wires=4)
+
+        cable.connect_port(port)
+
+        port_list = [print(i.name) for i in self.definition.ports]        
+
+        self.assertEqual(len(port_list), 1)
+
+        self.definition.split_port(port)
+
+        port_list = [print(i.name) for i in self.definition.ports]        
+
+        self.assertEqual(len(port_list), 4)
+
+
+    def test_duplicate_port(self):
+        port = self.definition.create_port( 
+        name="in0", is_downto=True, is_scalar = False, lower_index = 0, direction=sdn.IN, pins=4)
+
+        cable = self.definition.create_cable(name="in0",is_downto=True, is_scalar = False, lower_index = 0, wires=4)
+
+        cable.connect_port(port)
+
+        port_list = [print(i.name) for i in self.definition.ports]        
+
+        self.assertEqual(len(port_list), 1)
+
+        self.definition.duplicate_port(port)
+
+        port_list = [print(i.name) for i in self.definition.ports]        
+
+        self.assertEqual(len(port_list), 2)
+
+
+    def test_create_unconn_wires(self):
+        port = self.definition.create_port( 
+        name="in0", is_downto=True, is_scalar = False, lower_index = 0, direction=sdn.IN, pins=1)
+
+        cable_list = [print(i.name) for i in self.definition.cables]        
+
+        self.assertEqual(len(cable_list), 0)
+
+        self.definition.create_unconn_wires()
+
+        cable_list = [print(i.name) for i in self.definition.cables]        
+
+        self.assertEqual(len(cable_list), 1)
+
+
+
+
+
