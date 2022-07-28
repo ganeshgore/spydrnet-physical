9d8e97aa423577e628f3d274e83ff7f3e38b35e5
Ganesh Gore <goreganesh007@gmail.com>
2022-07-28 17:02:59 -0600

    Added comments for documentation

--- spydrnet_physical/composers/svg/composer.py
+++ spydrnet_physical/composers/svg/composer.py
@@ -8,18 +8,34 @@ import spydrnet as sdn
 
 
 class SVGComposer:
+    """
+    Class create SVG composer based on yosys and netlist SVG.
+    """
 
     def __init__(self):
+        # nothing to do during initialization
         pass
 
-    def run(self, netlist, file_out="out.svg"):
-        """ Main method to run composer """
-        verilog_file = "_"+file_out.replace(".svg", ".v")
+    # TODO
+    # Add option to suppress stdout printing of the yosys compilation
+    # Finish documentation and add in sphinx index
+    def run(self, netlist, yosys_cmmds="", svgout_file="out.svg"):
+        """
+        Main method to run composer
+
+        args:
+            netlist
+            yosys_cmmds
+            svgout_file
+
+        """
+        verilog_file = "_"+svgout_file.replace(".svg", ".v")
         sdn.compose(netlist, verilog_file, skip_constraints=True)
         top = netlist.top_instance.reference.name
 
-        json_file = "_"+file_out.replace(".svg", ".json")
+        json_file = "_"+svgout_file.replace(".svg", ".json")
         os.system(f"yosys -p 'prep -top {top}; " +
+                  yosys_cmmds +
                   f" write_json {json_file};' " +
                   f"{verilog_file}")
-        os.system(f"netlistsvg {json_file} -o {file_out};")
+        os.system(f"netlistsvg {json_file} -o {svgout_file};")
