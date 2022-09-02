3ee5fdc6c4f05c8ca5cf6eec9eb37fc21b77f6ad
Soniya Kiran <skiran@rslpt53.rapidsilicon.local>
2022-09-02 14:50:26 +0500

    WIP: added examples for Merging multiple instances and Creating feedthrough from multiple instances

--- examples/basic/07_multiple_instance_feedthrough.py
+++ examples/basic/07_multiple_instance_feedthrough.py
@@ -12,12 +12,40 @@ the feedthrough instead of creating independent feedthrough to pass through each
 """
 
 import logging
-
+from os import path
+from unicodedata import name
 import spydrnet as sdn
 import spydrnet_physical as sdnphy
+from spydrnet_physical.composers.html.composer import HTMLComposer
+from spydrnet_physical.composers.svg.composer import SVGComposer
 
 logger = logging.getLogger('spydrnet_logs')
 sdn.enable_file_logging(LOG_LEVEL='INFO')
 
-# TODO
-logger.warning("NotImplemented")
+#logger.warning("NotImplemented")
+
+
+netlist = sdnphy.load_netlist_by_name('basic_hierarchy')
+
+top = netlist.top_instance.reference
+inst1 = next(top.get_instances('inst_1_0'))
+inst2 = next(top.get_instances('inst_1_1'))
+
+
+composer = HTMLComposer()
+composer.run(netlist, file_out="1_mul_inst_ft_design.html")
+composer = SVGComposer()
+composer.run(netlist, file_out="1_mul_inst_ft_design.svg")
+
+cable = top.create_cable(name= 'A')
+wire = cable.create_wire()
+
+inst_list = [(cable,[inst1, inst2])]
+
+top.create_feedthrough_multiple(inst_list)
+top.create_unconn_wires()
+    
+composer = HTMLComposer()
+composer.run(netlist, file_out="2_mul_inst_ft_design.html")
+composer = SVGComposer()
+composer.run(netlist, file_out="2_mul_inst_ft_design.svg")
