61f769f9a1dbeba350230dd7b925d33471949788
Ganesh <goreganesh007@gmail.com>
2022-09-22 23:07:03 -0600

    Installed extra requirement

--- .github/build_env.sh
+++ .github/build_env.sh
@@ -7,5 +7,9 @@ if [ -f "requirements.txt" ]; then
     python3 -m pip install --upgrade \
     --no-cache-dir -r requirements.txt
 fi
+if [ -f "requirements_extra.txt" ]; then
+    python3 -m pip install --upgrade \
+    --no-cache-dir -r requirements_extra.txt
+fi
 echo "spydrnet_physical" > ~/.spydrnet
 python3 -c "import spydrnet as sdn;print(sdn.get_active_plugins());print(dir(sdn.ir.Cable))"
