2d9ac9fd64a098988a9b782382a18eaafd75baa5
Ganesh Gore <ggore@quicklogic.com>
2025-04-10 18:34:09 -0700

    Updated docker ubuntu

--- .github/build_docs.sh
+++ .github/build_docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-python3 -m venv buildenv
-source buildenv/bin/activate
+python3 -m venv /tmp/buildenv
+source /tmp/buildenv/bin/activate
 python3 -m pip install --upgrade --no-cache-dir pip
 if [ -f "docs/requirements.txt" ]; then
+    cat docs/requirements.txt | xargs -n 1 pip install
     python3 -m pip install --upgrade --no-cache-dir -r docs/requirements.txt
 fi
 pip install --upgrade Cython
 
-sed -i 's/tempclone\[2\]\[2\].*/tempclone[2][2] = this.key;/' /usr/lib/node_modules/netlistsvg/built/Cell.js 
+sed -i 's/tempclone\[2\]\[2\].*/tempclone[2][2] = this.key;/' /usr/lib/node_modules/netlistsvg/built/Cell.js
